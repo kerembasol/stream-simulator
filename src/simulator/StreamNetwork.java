11a50b60b860bac9445ef66e8b5e8c953877941e
kerem <kerem@abc.com>
2012-11-29 07:56:34 +0200

    initial commit

---
+++ src/simulator/StreamNetwork.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package simulator;
+
+import java.util.List;
+
+import entity.Node;
+import entity.Tracker;
+
+/**
+ * @author kerem
+ * 
+ */
+public class StreamNetwork {
+
+	private Tracker tracker;
+	private List<Node> watchingNodes;
+
+}
