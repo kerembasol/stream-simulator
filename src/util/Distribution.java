fe6b1efb33212c04088e5e65023ab8a6aa50b206
kerem <kerem@abc.com>
2012-11-29 07:56:46 +0200

    initial commit

--- src/util/Distribution.java
+++ src/util/Distribution.java
@@ -5,8 +5,17 @@ package util;
 
 /**
  * @author kerem
- *
+ * 
  */
 public class Distribution {
+	// TODO implement discrete exponential
+	public static int exponential(int parameter) {
+		return 1;
+	}
+
+	// TODO implement discrete poisson
+	public static int poisson(int parameter) {
+		return 1;
+	}
 
 }
