ce9b1a799dd7d6aef459299016e071d4ef7f1ebb
kerem <kerem@abc.com>
2012-12-13 07:09:59 +0200

    Fixed len playback update

--- src/entity/AdditionOfNewSetWithLargerSetSizeException.java
+++ src/entity/AdditionOfNewSetWithLargerSetSizeException.java
@@ -8,6 +8,11 @@ package entity;
  * 
  */
 public class AdditionOfNewSetWithLargerSetSizeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 708519445090957877L;
+
 	public AdditionOfNewSetWithLargerSetSizeException(String message) {
 		super(message);
 	}
